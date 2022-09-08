ee9816bd5b7269e3f02efe7d608985f3c6043979
BelloSaheed <st377126@gmail.com>
2022-09-08 15:37:12 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
- * main - Prints a text
- *
- * Return: Always 0 (Success)
+ * main - Prints the sizeof many var types
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float,  e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	Printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n', sizeof(d));
+	printf('Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
 }
