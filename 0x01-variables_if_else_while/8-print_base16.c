1348d9236791e4e1e49fa098d05c6b8d5895827e
BelloSaheed <st377126@gmail.com>
2022-09-11 10:30:39 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,6 @@
 int main(void)
 {	
 	char c;
-
 for (c = '0'; c <= '9'; c++)
 {
 	putchar(c);
@@ -17,7 +16,6 @@ for (c = '0'; c <= '9'; c++)
 for (c = 'a'; c <= 'f'; c++)
 {
 	putchar(c);
-
 }	
 putchar('\n');
 return (0);
