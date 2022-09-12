5812056aa8bdb5343c430f841f7ab3ba1669cdd9
BelloSaheed <st377126@gmail.com>
2022-09-12 16:54:49 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,8 +3,8 @@
 #include<time.h>
 /**
  *
- * main - print if the number is positive, zero or negative
- * this program prints "Programming is positive, zero and negative
+ * main - print if the numbers from 0-9
+ * this program prints: "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
  * return 0:
  */
 int main(void)
@@ -12,7 +12,9 @@ int main(void)
 	int d;
 
 	for (d = 0; d <= 9; d++)
+{
 	putchar(d);
+}
 	putchar('\n');
 return 0;
 }
