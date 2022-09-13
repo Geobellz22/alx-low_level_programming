96cf302d5694aea21d73c3f5e41cb3d5050b7292
BelloSaheed <st377126@gmail.com>
2022-09-13 16:34:44 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,10 @@
 /**
  * main - prints the alphabet in lowercase, followed by a new line
  */
- void print_alphabet(void);
+ void print_alphabet(void)
 {
 	char ch;
 	for (ch = 'a'; a <= 'z'; a++)
 		_putchar(a);
-	_putchar('\n')
+	_putchar('\n');
 }
