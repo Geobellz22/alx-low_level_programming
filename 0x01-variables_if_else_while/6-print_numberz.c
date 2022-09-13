5c9f2c9bccd42ec632863088389e6ab02f8d88f7
BelloSaheed <st377126@gmail.com>
2022-09-12 17:31:00 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,9 +3,11 @@
 #include<time.h>
 /**
  * main - print if the numbers from 0-9
+ * void: print if the numbers 0-9
  *
- * this program prints: "0,1,2,3,4,5,6,7,8,9"
- * return: 0;
+ * Description: using main function
+ * this program prints "0,1,2,3,4,5,6,7,8,9"
+ * Return: 0
  */
 int main(void)
 {
