00d0d15f954fba5e42cca115c58198f74c48dcc1
BelloSaheed <st377126@gmail.com>
2022-09-19 14:33:57 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/2-strien.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - length of string
+ * @s: input character
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
