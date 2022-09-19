8d29968088e2a66e6ec40f4ddf0aa1b1ba21726e
BelloSaheed <st377126@gmail.com>
2022-09-19 14:54:33 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,13 @@
 #include "main.h"
+
 /**
  * _puts - prints string
- * @str: input string
+ *  @str: input string
  * Return: no return
  */
- void _puts(char *str)
+
+void _puts(char *str)
+
 {
 	while (*str != 0)
 	{
