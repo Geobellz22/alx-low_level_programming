e49b5fd21b16590a02b65f462612e97c36ce77c7
BelloSaheed <st377126@gmail.com>
2022-09-20 09:31:21 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,12 @@ void print_rev(char *s)
 {
 	int c, i;
 
+	c = 0;
+
 	while (s[i] != 0)
 		i++;
 
-	for (i = c-1; i >= 0; i--)
+	for (i = c - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
