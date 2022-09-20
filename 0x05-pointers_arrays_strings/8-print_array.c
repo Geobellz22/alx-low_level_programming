df22a9915978797f0a93d52dea1a520b7cb4c3a8
BelloSaheed <st377126@gmail.com>
2022-09-20 14:41:39 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_array - prints half a string
+ * @a: array
+ * @n: length of array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		if (x != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	printf('\n');
+}
