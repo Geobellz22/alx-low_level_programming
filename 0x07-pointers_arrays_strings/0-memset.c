2da1e6c86fafa80a64a26740ab913a1a1b876002
BelloSaheed <st377126@gmail.com>
2022-09-28 15:20:07 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * _memset - memset function
- * @s: start point of string to change
+ * @s: start point ofstring to change
  * @b: value that will replace
  * @n: number of bytes to change
- * Return: changed pointer
+ * Return: change pointer
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *p = s;
-	
+
 	for (; n != 0; p++, n--)
 		*p = b;
 	return (s);
