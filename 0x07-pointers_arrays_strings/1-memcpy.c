9a65a9019e5c72eb16ceac7f8819912df39d89ed
BelloSaheed <st377126@gmail.com>
2022-09-28 15:44:44 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	char *dest1 = dest;
 	char *src1 = src;
 
-	for (; n != 0; dest1++, n--, src++)
+	for (; n != 0; dest1++, n--, src1++)
 		*dest1 = *src1;
 	return (dest);
 }
