aa45f5b9529cef07364c8c1764d729c25930bddb
BelloSaheed <st377126@gmail.com>
2022-09-30 14:55:37 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "min.h"
+/**
+ * factorial - factorial of a given numbber
+ * @n:int
+ * return:int
+ */
+int factorial(int n)
+{
+	int i;
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 1)
+	{
+		return (-1);
+	}
+	else
+	{
+		i = n *factorial(n - 1);
+	}
+	return (i);
+}
