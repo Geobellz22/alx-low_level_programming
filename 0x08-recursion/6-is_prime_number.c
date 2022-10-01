d83b796b6a14df6d8494edc8cf686e4b5489d2ab
BelloSaheed <st377126@gmail.com>
2022-10-01 01:26:32 -0700

    Task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * help  - helps prime function
+ * help - helps prime function
  * @n: number to be checked
  * @x: checks if n is divisible by x
- * Return: o if not prime, 1 if prime
+ * Return: 0 if not prime, 1 if prime
  */
 int help(int n, int x)
 {
