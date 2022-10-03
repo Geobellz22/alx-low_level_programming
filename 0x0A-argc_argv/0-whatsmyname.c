9a26b09577072ae7926aa7b7ccf93fb57898b315
BelloSaheed <st377126@gmail.com>
2022-10-03 14:53:57 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - program prints its name, followed by a new line
+ * @argc: number of arguments to be counted on the program
+ * @argv: lists of arguments passed on the function
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0])
+	return (0);
+}	
