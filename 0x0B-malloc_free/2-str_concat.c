5b823ae0491cc7a1afe5cfe6ebc2eb6ef4d27a87
BelloSaheed <st377126@gmail.com>
2022-10-04 18:47:21 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,7 @@ char *str_concat(char *s1, char *s2)
 	str = malloc(sizeof(char) * (i + j + 1));
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; str[i]; i++)
+	for (i = 0; s1[i]; i++)
 		*(str + i) = *(s1 + i);
 	for (j = 0; s2[j]; j++)
 	{
